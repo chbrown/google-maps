5a0e2db79b87d5009d2ecf23082bb847905439e8
Christopher Brown <io@henrian.com>
2013-06-16 21:56:10 -0500

    Basic drawing and persistence

---
+++ README.md
@@ -0,0 +1,25 @@
+## API
+
+Google API Key:
+
+    AIzaSyCDYwRP0wOSFFgzJDr73JtketDW335C234
+
+## Links
+
+Sources
+
+* https://developers.google.com/maps/documentation/javascript/
+* http://mapscripting.com/how-to-use-geolocation-in-mobile-safari
+
+Alternatives
+
+* http://boundingbox.klokantech.com/
+
+Other cool maps
+
+* https://github.com/moshen/node-googlemaps.git
+* http://hpneo.github.io/gmaps/
+
+## License
+
+Copyright © 2013 Christopher Brown. [MIT Licensed](LICENSE).
---
+++ google-maps.js
@@ -0,0 +1,69 @@
+function KeyState() {
+  var down = this.down = {};
+
+  window.addEventListener('keydown', function(ev) {
+    down[ev.which] = true;
+  }, false);
+
+  window.addEventListener('keyup', function(ev) {
+    down[ev.which] = false;
+  }, false);
+}
+
+function getPosition(callback) {
+  // simply flatten the weird dual-callback getCurrentPosition API
+  // to the normal function(err, ...) paradigm.
+  navigator.geolocation.getCurrentPosition(function(position) {
+    callback(null, position);
+  }, callback);
+}
+
+var opts_frozen = {
+  draggable: false,
+  zoomControl: false,
+  scrollwheel: false,
+  disableDoubleClickZoom: true
+};
+
+var opts_interactive = {
+  draggable: true,
+  zoomControl: true,
+  scrollwheel: true,
+  disableDoubleClickZoom: false
+};
+
+var google_map_events = [
+  'bounds_changed',
+  'center_changed',
+  'click',
+  'dblclick',
+  'drag',
+  'dragend',
+  'dragstart',
+  'heading_changed',
+  'idle',
+  'maptypeid_changed',
+  'mousemove',
+  'mouseout',
+  'mouseover',
+  'projection_changed',
+  'resize',
+  'rightclick',
+  'tilesloaded',
+  'tilt_changed',
+  'zoom_changed',
+  // undocumented, but otherwise standard DOM-level-2 MouseEvents:
+  'mousedown',
+  'mouseup'
+];
+
+function eavesdrop(emitter, events) {
+  events.forEach(function(event) {
+    emitter.addListener(event, function() {
+      console.info(event, arguments);
+      window.last_event = event;
+    });
+  });
+}
+// eavesdrop(map, google_map_events);
+
